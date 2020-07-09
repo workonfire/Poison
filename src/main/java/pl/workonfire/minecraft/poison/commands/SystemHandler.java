b35ebbf20000b0970ff7754240ef6ca4bcf8bd52
workonfire <34654573+workonfire@users.noreply.github.com>
2020-07-09 23:58:44 +0200

    Thread-safety

--- src/main/java/pl/workonfire/minecraft/poison/commands/SystemHandler.java
+++ src/main/java/pl/workonfire/minecraft/poison/commands/SystemHandler.java
@@ -1,6 +1,5 @@
 package pl.workonfire.minecraft.poison.commands;
 
-import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -17,29 +16,27 @@ public class SystemHandler implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (args.length > 0) {
-            Bukkit.getScheduler().runTaskAsynchronously(Poison.getInstance(), () -> {
-                String systemCommand = String.join(" ", args);
-                long startTime = System.nanoTime();
-                sender.sendMessage(Poison.getPrefix() + "§cExecuting system command §7§o" + systemCommand + "§r...");
-                try {
-                    String commandInterpreter;
-                    if (System.getProperty("os.name").contains("Windows")) commandInterpreter = "cmd /c";
-                    else commandInterpreter = "bash -c";
-                    Process process = Runtime.getRuntime().exec(commandInterpreter + " " + systemCommand);
-                    process.waitFor();
-                    BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-                    List<String> output = new ArrayList<>();
-                    String line;
-                    while ((line = reader.readLine()) != null) output.add(line + "\n");
-                    if (!output.isEmpty()) for (String outputLine : output) sender.sendMessage(outputLine);
-                    long stopTime = System.nanoTime();
-                    sender.sendMessage(Poison.getPrefix() + "§aCommand execution finished in §f" + (stopTime - startTime) / 1000000 + " ms");
-                } catch (IOException | InterruptedException exception) {
-                    sender.sendMessage(Poison.getPrefix() + "§4Error.");
-                }
-            });
+            String systemCommand = String.join(" ", args);
+            long startTime = System.nanoTime();
+            sender.sendMessage(Poison.getPrefix() + "§cExecuting system command §7§o" + systemCommand + "§c... A lag may occur.");
+            try {
+                String commandInterpreter;
+                if (System.getProperty("os.name").contains("Windows")) commandInterpreter = "cmd /c";
+                else commandInterpreter = "bash -c";
+                Process process = Runtime.getRuntime().exec(commandInterpreter + " " + systemCommand);
+                process.waitFor();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+                List<String> output = new ArrayList<>();
+                String line;
+                while ((line = reader.readLine()) != null) output.add(line + "\n");
+                if (!output.isEmpty()) for (String outputLine : output) sender.sendMessage(outputLine);
+                long stopTime = System.nanoTime();
+                sender.sendMessage(Poison.getPrefix() + "§aCommand execution finished in §f" + (stopTime - startTime) / 1000000 + " ms");
+            } catch (IOException | InterruptedException exception) {
+                sender.sendMessage(Poison.getPrefix() + "§4Error.");
+            }
         }
-        else sender.sendMessage("§4§l§oPoison §fv" + Poison.getPluginVersion());
+        else sender.sendMessage("§4§l§oPOISON §fv" + Poison.getPluginVersion());
         return true;
     }
 }
